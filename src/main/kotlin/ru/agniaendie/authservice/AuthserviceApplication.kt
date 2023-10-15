45b386f8a3f981aff087549a81ec59942c760d64
AgniaEndie <Endienasg3@gmail.com>
2023-10-15 16:50:26 +0500

    Start Development

---
+++ src/main/kotlin/ru/agniaendie/authservice/AuthserviceApplication.kt
@@ -0,0 +1,11 @@
+package ru.agniaendie.authservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthserviceApplication>(*args)
+}
---
+++ src/test/kotlin/ru/agniaendie/authservice/AuthserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.agniaendie.authservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
